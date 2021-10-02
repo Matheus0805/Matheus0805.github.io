83a20a9f45fd9e0b42cea1c559eaea5e4654d318
Matheus Landim <72341602+Matheus0805@users.noreply.github.com>
2021-10-02 00:29:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function ConverterDolar() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmRealNumerico = parseFloat(valor);
+  
+    var valorEmDolar = valorEmRealNumerico * 5;
+    console.log(valorEmDolar);
+  
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "$" + valorEmDolar;
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }
+
+  function ConverterEuro() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmRealNumerico = parseFloat(valor);
+  
+    var valorEmEuro = valorEmRealNumerico * 6.22;
+    console.log(valorEmEuro);
+  
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "€" + valorEmEuro;
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }
+
+  function ConverterIene() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmRealNumerico = parseFloat(valor);
+  
+    var valorEmIene = valorEmRealNumerico * 0.048;
+    console.log(valorEmIene);
+  
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "¥" + valorEmIene;
+    elementoValorConvertido.innerHTML = valorConvertido;
+  }
